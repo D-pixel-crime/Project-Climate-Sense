90ea42e6efcae319934241e0113ebab20ff832af
D-pixel-crime <119526848+D-pixel-crime@users.noreply.github.com>
2024-01-10 23:24:56 +0530

    Update main.js

--- Project Climate Sense/public/functionality/main.js
+++ Project Climate Sense/public/functionality/main.js
@@ -1,9 +1,4 @@
 $(document).ready(function () {
-    var cityName = $(".city-name").text().trim();
-    $(".city-name").css('animation', `typing 1s steps(${cityName.length})`)
-});
-
-$(document).ready(function () {
     $(".weather-boxes").addClass("slide-down");
     setTimeout(() => {
         $(".weather-boxes").removeClass("slide-down");
